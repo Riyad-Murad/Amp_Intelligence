f7d7eb8e689bfa90ef9548d27576d1fec3520ec4
Riyad Murad <riyadmurad01@gmail.com>
2025-04-28 23:52:45 +0300

    Laravel_Structure: Added Empty Test files with api route

---
+++ Laravel_Amp_Intelligence/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
+
+Route::group(["prefix" => "v1"], function () {
+    //Authenticated Users
+    Route::group(["middleware" => "auth:api"], function () {
+
+    });
+
+    //Unauthenticated Users
+    // Route::post("/login", [AuthController::class, "login"])->name("login");
+    // Route::post("/signup", [AuthController::class, "signup"])->name("signup");
+});
