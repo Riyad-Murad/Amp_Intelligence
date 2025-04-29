041d25ba7200936228a0f5c37af63c1422bc4fc8
Riyad Murad <riyadmurad01@gmail.com>
2025-04-29 17:40:31 +0300

    Laravel_Structure: Added fillable fields for the Models

--- Laravel_Amp_Intelligence/app/Models/Line.php
+++ Laravel_Amp_Intelligence/app/Models/Line.php
@@ -9,6 +9,15 @@ class Line extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'voltage_l1',
+        'voltage_l2',
+        'voltage_l3',
+        'power_l1',
+        'power_l2',
+        'power_l3',
+    ];
+
     public function master()
     {
         return $this->belongsTo(Master::class);
