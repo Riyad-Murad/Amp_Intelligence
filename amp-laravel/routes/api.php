8b5adb873b621435aef1308acb5d2c41fda9585c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 19:51:20 +0300

    laravel_apis: Added the needed api structure
    added the apis under the middlewares and added the Controllers for each Route

--- amp-laravel/routes/api.php
+++ amp-laravel/routes/api.php
@@ -2,23 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Common\AuthController;
+use App\Http\Controllers\Client\MetricsController;
+use App\Http\Controllers\Client\ClientCheckinController;
+use App\Http\Controllers\Provider\LinesController;
+use App\Http\Controllers\Provider\ProviderCheckinController;
 
 Route::group(["prefix" => "v1"], function () {
     //Authenticated Users
     Route::group(["middleware" => "auth:api"], function () {
         // Client Users
-        Route::group(["prefix" => "clients", "middleware" => "isClient"], function () {
-
-        });
+        Route::group(["prefix" => "clients", "middleware" => "isClient"], function () {});
 
         // Slave Users
         Route::group(["prefix" => "slaves", "middleware" => "isSlave"], function () {
-
+            Route::post("/checkin", [ClientCheckinController::class, "slave-checkin"]);
+            Route::post("/metrics", [MetricsController::class, "slave-metrics"]);
         });
 
         // Master Users
         Route::group(["prefix" => "masters", "middleware" => "isMaster"], function () {
-
+            Route::post("/checkin", [ProviderCheckinController::class, "master-checkin"]);
+            Route::post("/lines", [LinesController::class, "master-lines"]);
         });
     });
 
