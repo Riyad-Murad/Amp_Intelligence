ff0bc0f8a3cd98ed837704138ab11043d57e7f2a
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 23:52:12 +0300

    laravel_apis: Modified try-catch block and added more Exception handling

--- amp-laravel/app/Http/Controllers/Client/ClientCheckinController.php
+++ amp-laravel/app/Http/Controllers/Client/ClientCheckinController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use Throwable;
+use InvalidArgumentException;
 use App\Http\Controllers\Controller;
 use App\Services\Client\ClientCheckinService;
 use App\Http\Requests\Client\ClientRequestCheckin;
@@ -12,10 +13,15 @@ class ClientCheckinController extends Controller
     public function slaveCheckin(ClientRequestCheckin $request)
     {
         try {
-            $slave = ClientCheckinService::checkin($request->all());
-            return $this->messageResponse(true, "Slave checked in successfully", 200, $slave);
+            $slave = ClientCheckinService::checkin($request->validated());
+
+            return $this->messageResponse(true, "Slave Checkin Successful", 200, $slave);
+        } catch (InvalidArgumentException $e) {
+            return $this->errorMessageResponse(false, 'Invalid Input', $e->getMessage(), 422);
         } catch (Throwable $e) {
             return $this->errorMessageResponse(false, "Slave checkin failed", $e->getMessage(), 500);
+        } catch (\Exception $e) {
+            return $this->errorMessageResponse(false, 'Unexpected error', $e->getMessage(), 500);
         }
     }
 }
