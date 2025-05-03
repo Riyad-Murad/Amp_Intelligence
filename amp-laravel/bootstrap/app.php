029f2e27af97212fbf0c89821182baf254c432b8
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 04:19:28 +0300

    laravel_apis: Added the Middleware aliases

--- amp-laravel/bootstrap/app.php
+++ amp-laravel/bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\EnsureAdmin;
+use App\Http\Middleware\EnsureClient;
+use App\Http\Middleware\EnsureProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            "isClient" => EnsureClient::class,
+            "isProvider" => EnsureProvider::class,
+            "isAdmin" => EnsureAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
