73fb370e8d44f6f6cfd20dcfde742eba6a8457b2
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:19:11 +0300

    laravel_apis: Modified the ProviderCheckinController to execute the api

--- amp-laravel/app/Http/Controllers/Provider/ProviderCheckinController.php
+++ amp-laravel/app/Http/Controllers/Provider/ProviderCheckinController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Provider;
 
 use Throwable;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\Provider\ProviderCheckinService;
+use App\Http\Requests\Provider\ProviderRequestCheckin;
 
 class ProviderCheckinController extends Controller
 {
-    //
+    public function masterCheckin(ProviderRequestCheckin $request)
+    {
+        try {
+            $data = ProviderCheckinService::checkin($request->all());
+            return $this->messageResponse(true, "Master checked in successfully", 200, $data);
+        } catch (Throwable $e) {
+            return $this->errorMessageResponse(false, "Master checkin failed", $e->getMessage(), 500);
+        }
+    }
 }
