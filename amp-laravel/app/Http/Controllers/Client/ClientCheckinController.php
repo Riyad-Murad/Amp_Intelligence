9d44ed767a5ac6e2140eb3f98ee20092bed335f1
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:20:44 +0300

    laravel_apis: Modified the ClientCheckinController to execute the api

--- amp-laravel/app/Http/Controllers/Client/ClientCheckinController.php
+++ amp-laravel/app/Http/Controllers/Client/ClientCheckinController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Client;
 
 use Throwable;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\Client\ClientCheckinService;
+use App\Http\Requests\Client\ClientRequestCheckin;
 
 class ClientCheckinController extends Controller
 {
-    //
+    public function slaveCheckin(ClientRequestCheckin $request)
+    {
+        try {
+            $slave = ClientCheckinService::checkin($request->all());
+            return $this->messageResponse(true, "Slave checked in successfully", 200, $slave);
+        } catch (Throwable $e) {
+            return $this->errorMessageResponse(false, "Slave checkin failed", $e->getMessage(), 500);
+        }
+    }
 }
