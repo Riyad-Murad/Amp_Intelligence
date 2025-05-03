5fffc36f91ea998a766f0444eac8510f04042df4
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:23:17 +0300

    laravel_apis: Modified the MetricsController to execute the api

--- amp-laravel/app/Http/Controllers/Client/MetricsController.php
+++ amp-laravel/app/Http/Controllers/Client/MetricsController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Client;
 
 use Throwable;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\Client\MetricsEntryService;
+use App\Http\Requests\Client\ClientRequestMetric;
 
 class MetricsController extends Controller
 {
-    //
+    public function slaveMetrics(ClientRequestMetric $request)
+    {
+        try {
+            $metric = MetricsEntryService::addMetrics($request->all());
+            return $this->messageResponse(true, "Slave metric stored", 200, $metric);
+        } catch (Throwable $e) {
+            return $this->errorMessageResponse(false, "Metric save failed", $e->getMessage(), 500);
+        }
+    }
 }
