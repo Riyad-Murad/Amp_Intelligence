aa0f1a7986874928f5e8cf3149d75a472dcfa56a
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:21:48 +0300

    laravel_apis: Added the Checkin code for LinesEntryService

--- amp-laravel/app/Services/Provider/LinesEntryService.php
+++ amp-laravel/app/Services/Provider/LinesEntryService.php
@@ -2,13 +2,23 @@
 
 namespace App\Services\Provider;
 
+use App\Models\Master;
+
 class LinesEntryService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function addLines(array $data)
     {
-        //
+        $master = Master::where('name', $data['masterId'])->firstOrFail();
+
+        $line = $master->lines()->create([
+            'voltage_l1' => $data['voltageL1'],
+            'voltage_l2' => $data['voltageL2'],
+            'voltage_l3' => $data['voltageL3'],
+            'power_l1' => $data['powerL1'],
+            'power_l2' => $data['powerL2'],
+            'power_l3' => $data['powerL3'],
+        ]);
+
+        return $line->toArray();
     }
 }
