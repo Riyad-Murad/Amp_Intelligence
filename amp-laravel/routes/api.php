72fe7a4aa9aa7bddcd3dcaab1590939232e6a10c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-04 02:36:22 +0300

    laravel_apis: Removed comments

--- amp-laravel/routes/api.php
+++ amp-laravel/routes/api.php
@@ -6,25 +6,15 @@ use App\Http\Controllers\Admin\MetricsController;
 use App\Http\Controllers\Admin\ClientCheckinController;
 use App\Http\Controllers\Admin\LinesController;
 use App\Http\Controllers\Admin\ProviderCheckinController;
-// use App\Http\Controllers\Client\MetricsController;
-// use App\Http\Controllers\Client\ClientCheckinController;
-// use App\Http\Controllers\Provider\LinesController;
-// use App\Http\Controllers\Provider\ProviderCheckinController;
 
 Route::group(["prefix" => "v1"], function () {
     //Authenticated Users
     Route::group(["middleware" => "auth:api"], function () {
         // Slave/Client Users
-        Route::group(["prefix" => "slaves", "middleware" => "isClient"], function () {
-            // Route::post("/checkin", [ClientCheckinController::class, "slaveCheckin"]);
-            // Route::post("/metrics", [MetricsController::class, "slaveMetrics"]);
-        });
-        
+        Route::group(["prefix" => "slaves", "middleware" => "isClient"], function () {});
+
         // Master/Provider Users
-        Route::group(["prefix" => "masters", "middleware" => "isProvider"], function () {
-            // Route::post("/checkin", [ProviderCheckinController::class, "masterCheckin"]);
-            // Route::post("/lines", [LinesController::class, "masterLines"]);
-        });
+        Route::group(["prefix" => "masters", "middleware" => "isProvider"], function () {});
 
         // Admin Users
         Route::group(["prefix" => "admins", "middleware" => "isAdmin"], function () {
