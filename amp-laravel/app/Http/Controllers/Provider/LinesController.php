c725aecb48f5acd64b7ae61f5da93ac5ddfb0900
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 20:05:04 +0300

    laravel_apis: Added the LinesEntryService to the headers

--- amp-laravel/app/Http/Controllers/Provider/LinesController.php
+++ amp-laravel/app/Http/Controllers/Provider/LinesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Provider;
 
-use App\Http\Controllers\Controller;
+use Throwable;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Provider\LinesEntryService;
 
 class LinesController extends Controller
 {
