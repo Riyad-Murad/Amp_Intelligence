155f515c91c750f53a4ad9e18f91c76bb3c68d56
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 20:03:36 +0300

    laravel_apis: Added the MetricsEntryService to the headers

--- amp-laravel/app/Http/Controllers/Client/MetricsController.php
+++ amp-laravel/app/Http/Controllers/Client/MetricsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Client;
 
-use App\Http\Controllers\Controller;
+use Throwable;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Client\MetricsEntryService;
 
 class MetricsController extends Controller
 {
