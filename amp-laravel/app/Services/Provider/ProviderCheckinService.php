9fcf8ec7a218400cd621909267254f9a6857fd4c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:18:07 +0300

    laravel_apis: Added the Checkin code for ProviderCheckinService

--- amp-laravel/app/Services/Provider/ProviderCheckinService.php
+++ amp-laravel/app/Services/Provider/ProviderCheckinService.php
@@ -2,13 +2,17 @@
 
 namespace App\Services\Provider;
 
+use App\Models\Master;
+
 class ProviderCheckinService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function checkin(array $data)
     {
-        //
+        $master = Master::firstOrCreate(
+            ['name' => $data['masterId']],
+            ['user_id' => $data['userId']]
+        );
+
+        return $master->toArray();
     }
 }
