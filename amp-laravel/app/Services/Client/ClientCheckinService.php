e51095ffd2e6ce432c0857c1d957ebf79cad9ea9
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 20:01:34 +0300

    laravel_apis: Created the ClientCheckinService
    It contains the function to add entries to the Slaves table

---
+++ amp-laravel/app/Services/Client/ClientCheckinService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Client;
+
+class ClientCheckinService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
