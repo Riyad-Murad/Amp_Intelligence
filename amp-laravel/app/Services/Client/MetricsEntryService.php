2523da3e18ccf3918537019f54890bafbef5408b
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 21:22:58 +0300

    laravel_apis: Added the Metric code for MetricsEntryService

--- amp-laravel/app/Services/Client/MetricsEntryService.php
+++ amp-laravel/app/Services/Client/MetricsEntryService.php
@@ -2,13 +2,26 @@
 
 namespace App\Services\Client;
 
+use App\Models\Slave;
+use App\Models\Master;
+use App\Models\Metric;
+
 class MetricsEntryService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function addMetrics(array $data)
     {
-        //
+        $master = Master::where('name', $data['masterId'])->firstOrFail();
+        $slave = Slave::where('modbus_id', $data['modbusId'])->firstOrFail();
+
+        $metric = Metric::create([
+            'voltage' => $data['voltage'],
+            'current' => $data['current'],
+            'power' => $data['power'],
+            'energy' => $data['energy'],
+            'slave_id' => $slave->id,
+            'master_id' => $master->id,
+        ]);
+
+        return $metric->toArray();
     }
 }
