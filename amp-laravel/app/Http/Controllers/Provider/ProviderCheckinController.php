73f569a3b24571fc892c95618e043a2c8d143016
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 20:04:20 +0300

    laravel_apis: Added the ProviderCheckinService to the headers

--- amp-laravel/app/Http/Controllers/Provider/ProviderCheckinController.php
+++ amp-laravel/app/Http/Controllers/Provider/ProviderCheckinController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Provider;
 
-use App\Http\Controllers\Controller;
+use Throwable;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Provider\ProviderCheckinService;
 
 class ProviderCheckinController extends Controller
 {
