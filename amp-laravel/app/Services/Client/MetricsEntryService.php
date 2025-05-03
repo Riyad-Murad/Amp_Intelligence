2cdcfbb3a8dca00da538100e3d61d14e1db74d5c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-03 20:03:03 +0300

    laravel_apis: Created the MetricsEntryService
    It contains the function to add entries to the Metrics table

---
+++ amp-laravel/app/Services/Client/MetricsEntryService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Client;
+
+class MetricsEntryService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
