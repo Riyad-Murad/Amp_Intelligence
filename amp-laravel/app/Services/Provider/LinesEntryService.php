79b9037503d21b9e16b2e93f4efccfee98d35c92
Riyad Murad <riyadmurad01@gmail.com>
2025-05-04 02:39:18 +0300

    laravel_apis: Added the LineEntryService code

--- amp-laravel/app/Services/Provider/LinesEntryService.php
+++ amp-laravel/app/Services/Provider/LinesEntryService.php
@@ -6,17 +6,17 @@ use App\Models\Master;
 
 class LinesEntryService
 {
-    public static function addLines(array $data)
+    public static function addLines(array $data): array
     {
-        $master = Master::where('name', $data['master_id'])->firstOrFail();
+        $master = Master::where('id', $data['master_id'])->firstOrFail();
 
         $line = $master->lines()->create([
-            'voltage_l1' => $data['voltageL1'],
-            'voltage_l2' => $data['voltageL2'],
-            'voltage_l3' => $data['voltageL3'],
-            'power_l1' => $data['powerL1'],
-            'power_l2' => $data['powerL2'],
-            'power_l3' => $data['powerL3'],
+            'voltage_l1' => $data['voltage_l1'],
+            'voltage_l2' => $data['voltage_l2'],
+            'voltage_l3' => $data['voltage_l3'],
+            'power_l1'   => $data['power_l1'],
+            'power_l2'   => $data['power_l2'],
+            'power_l3'   => $data['power_l3'],
         ]);
 
         return $line->toArray();
