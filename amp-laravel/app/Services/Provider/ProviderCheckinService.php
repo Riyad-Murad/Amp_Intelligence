7a630b056f6c808b8f20db1c205842352e32f658
Riyad Murad <riyadmurad01@gmail.com>
2025-05-04 19:48:05 +0300

    laravel_apis: Added a code block to add a user of type Provider on Master Checkin

--- amp-laravel/app/Services/Provider/ProviderCheckinService.php
+++ amp-laravel/app/Services/Provider/ProviderCheckinService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services\Provider;
 
+use App\Models\User;
 use App\Models\Master;
-use InvalidArgumentException;
 
 class ProviderCheckinService
 {
@@ -13,6 +13,12 @@ class ProviderCheckinService
             ['name' => $data['name']],
             ['user_id' => $data['user_id']]
         );
+
+        if ($master->wasRecentlyCreated) {
+            User::create([
+                'user_type' => 'Provider',
+            ]);
+        }
 
         return $master->toArray();
     }
