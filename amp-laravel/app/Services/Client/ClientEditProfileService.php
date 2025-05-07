791c60cc5351e066024bfe148c415fa928914f42
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 02:00:49 +0300

    Client_Controller_Functions: Added if condition to check if the "name" attribute exist

--- amp-laravel/app/Services/Client/ClientEditProfileService.php
+++ amp-laravel/app/Services/Client/ClientEditProfileService.php
@@ -2,13 +2,20 @@
 
 namespace App\Services\Client;
 
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
 class ClientEditProfileService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function editProfile(array $data)
     {
-        //
+        $user = JWTAuth::user();
+
+        if (isset($data['name'])) {
+            $user->name = $data['name'];
+        }
+
+        $user->save();
+
+        return $user;
     }
 }
