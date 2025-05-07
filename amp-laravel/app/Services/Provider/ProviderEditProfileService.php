47e2be4d43952008c7ec49d6586a8557e126228f
Riyad Murad <riyadmurad01@gmail.com>
2025-05-07 23:54:03 +0300

    laravel_all_api_structure: Created Service to Edit Provider Profile for Providers

---
+++ amp-laravel/app/Services/Provider/ProviderEditProfileService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Provider;
+
+class ProviderEditProfileService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
