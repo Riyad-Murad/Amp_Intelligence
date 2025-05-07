d67a547c109054172b569b5297ae9824efbe4e50
Riyad Murad <riyadmurad01@gmail.com>
2025-05-07 22:48:45 +0300

    laravel_all_apis_structure: Added "getAllContactMessages" Route for Admins

--- amp-laravel/routes/api.php
+++ amp-laravel/routes/api.php
@@ -38,7 +38,7 @@ Route::group(["prefix" => "v1"], function () {
         // Admin Users
         Route::group(["prefix" => "admins", "middleware" => "isAdmin"], function () {
             Route::get("/getAllProviders", [AdminFunctionsController::class, "getProviders"]);
-
+            Route::get("/getAllContactMessages", [AdminFunctionsController::class, "getContactMessages"]);
         });
     });
 
