59f367963ec467c7d4adc95c8576cf5995bbba79
Riyad Murad <riyadmurad01@gmail.com>
2025-05-07 23:53:20 +0300

    laravel_all_api_structure: Created Service to Edit Profile for Admins

---
+++ amp-laravel/app/Services/Admin/AdminEditProfileService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin;
+
+class AdminEditProfileService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
