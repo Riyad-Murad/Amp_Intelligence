8214476d4c0a20cc7ece384d22144ff7b0570e96
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 02:43:32 +0300

    Admin_Controller_Functions: Added verification rules for "editProfile" for Admins

--- amp-laravel/app/Http/Requests/Admin/EditProfileRequest.php
+++ amp-laravel/app/Http/Requests/Admin/EditProfileRequest.php
@@ -11,7 +11,7 @@ class EditProfileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class EditProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|email|max:255',
+            'password' => 'sometimes|nullable|string|min:6',
+            'phone' => 'sometimes|required|string|max:20',
         ];
     }
 }
