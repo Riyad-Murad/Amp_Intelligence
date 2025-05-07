a77faa7faf99b57d3439eefe56760d2258b6d160
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 02:27:08 +0300

    Admin_Controller_Functions: Created Edit Profile Service for Admins

---
+++ amp-laravel/app/Services/Admin/editProfileService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Admin;
+
+class editProfileService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
