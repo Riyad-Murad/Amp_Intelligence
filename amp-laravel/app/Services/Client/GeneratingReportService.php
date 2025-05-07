9f43cf7a9bb8e497849d404434c072ad6d8cd9e9
Riyad Murad <riyadmurad01@gmail.com>
2025-05-07 20:48:32 +0300

    client_generate_report: Initialized the Prism Structured response

--- amp-laravel/app/Services/Client/GeneratingReportService.php
+++ amp-laravel/app/Services/Client/GeneratingReportService.php
@@ -54,5 +54,11 @@ class GeneratingReportService
             ],
             requiredFields: ['summary', 'voltageInsights', 'powerInsights', 'energyInsights']
         );
+
+        $response = Prism::structured()
+            ->using(Provider::OpenAI, 'o4-mini')
+            ->withSchema($schema)
+            ->withPrompt($prompt)
+            ->asStructured();
     }
 }
