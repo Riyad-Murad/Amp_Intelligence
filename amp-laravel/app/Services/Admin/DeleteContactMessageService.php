80c20785b87567850109ae0ac934cc40e9a841cd
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 03:55:11 +0300

    Admin_Controller_Functions: Added condition to check if "message" variable is found in the request to delete it otherwise throw an exception

--- amp-laravel/app/Services/Admin/DeleteContactMessageService.php
+++ amp-laravel/app/Services/Admin/DeleteContactMessageService.php
@@ -2,13 +2,19 @@
 
 namespace App\Services\Admin;
 
+use App\Models\ContactForm;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class DeleteContactMessageService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function delete($id): bool
     {
-        //
+        $message = ContactForm::find($id);
+
+        if (!$message) {
+            throw new ModelNotFoundException("Contact message with ID {$id} not found.");
+        }
+
+        return $message->delete();
     }
 }
