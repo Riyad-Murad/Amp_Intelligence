790ab81466687f0483fe2563845d30e0f9dd8b97
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 04:40:58 +0300

    Provider_Controller_Functions: Declared user variable to fetch the Client based on given id and user_type Client

--- amp-laravel/app/Services/Provider/EditClientUserService.php
+++ amp-laravel/app/Services/Provider/EditClientUserService.php
@@ -2,13 +2,32 @@
 
 namespace App\Services\Provider;
 
+use App\Models\User;
+
 class EditClientUserService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function editUser(int $id, array $data)
     {
-        //
+        $user = User::where('id', $id)->where('user_type', 'Client')->firstOrFail();        
+
+        if (isset($data['name'])) {
+            $user->name = $data['name'];
+        }
+
+        if (isset($data['email'])) {
+            $user->email = $data['email'];
+        }
+
+        if (!empty($data['password'])) {
+            $user->password = bcrypt($data['password']);
+        }
+
+        if (isset($data['phone_number'])) {
+            $user->phone_number = $data['phone_number'];
+        }
+
+        $user->save();
+
+        return $user;
     }
 }
