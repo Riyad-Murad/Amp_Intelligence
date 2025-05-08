200cd0eec238c2a0ca4d0e4604bc33368d0e5a92
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 03:26:57 +0300

    Admin_Controller_Functions: Created Service to edit providers profiles for Admins

---
+++ amp-laravel/app/Services/Admin/AdminEditProviderProfileService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Admin;
+
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AdminEditProviderProfileService
+{
+    public static function editProfile(array $data){
+
+    }
+}
