eae900f9c5e41f9ec31a4aa6c236900535d44573
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 01:45:56 +0300

    landing_page_hero_section_dev: Added HerSection Component to Home Landing Page

--- amp-client/src/Pages/Home/Home.jsx
+++ amp-client/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import './styles.css'
-import React from 'react'
-import Navbar from '../../Components/LandingPageComponents/Navbar/Navbar'
+import "./styles.css";
+import React from "react";
+import Navbar from "../../Components/LandingPageComponents/Navbar/Navbar";
+import HeroSection from "../../Components/LandingPageComponents/HeroSection/HeroSection";
 
 const Home = () => {
   return (
-    <Navbar />
-  )
-}
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+};
 
-export default Home
+export default Home;
