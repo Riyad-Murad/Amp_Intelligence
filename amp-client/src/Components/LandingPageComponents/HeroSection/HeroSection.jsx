864884d44b9053436a29aaf3d6706245870d4278
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 01:50:47 +0300

    landing_page_hero_section_dev: Added HeroSection Component Code

--- amp-client/src/Components/LandingPageComponents/HeroSection/HeroSection.jsx
+++ amp-client/src/Components/LandingPageComponents/HeroSection/HeroSection.jsx
@@ -1,10 +1,13 @@
-import "./styles.css"
-import React from 'react'
+import "./styles.css";
+import React from "react";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className="hero-section">
+      <div className="hero-overlay"></div>
+      <h1>Looking for a way to provision and manage your Electrical usage?</h1>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
