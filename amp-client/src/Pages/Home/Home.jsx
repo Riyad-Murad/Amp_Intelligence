3bfb829b9dc126fdbf896b71617cfe3e20083693
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 02:15:34 +0300

    landing_about_us_section_dev: Added the AboutUsSection the the Home Landing Page

--- amp-client/src/Pages/Home/Home.jsx
+++ amp-client/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import "./styles.css";
 import React from "react";
 import Navbar from "../../Components/LandingPageComponents/Navbar/Navbar";
 import HeroSection from "../../Components/LandingPageComponents/HeroSection/HeroSection";
+import AboutUsSection from "../../Components/LandingPageComponents/AboutUs/AboutUsSection";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutUsSection />
     </>
   );
 };
