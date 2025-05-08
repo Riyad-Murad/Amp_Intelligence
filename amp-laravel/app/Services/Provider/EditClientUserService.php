c6771155a611337d1fb40703ccd446a96cd2d489
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 04:41:48 +0300

    Provider_Controller_Functions: : Added condition to check if "name" is found in the request to change it

--- amp-laravel/app/Services/Provider/EditClientUserService.php
+++ amp-laravel/app/Services/Provider/EditClientUserService.php
@@ -8,22 +8,10 @@ class EditClientUserService
 {
     public static function editUser(int $id, array $data)
     {
-        $user = User::where('id', $id)->where('user_type', 'Client')->firstOrFail();        
+        $user = User::where('id', $id)->where('user_type', 'Client')->firstOrFail();
 
         if (isset($data['name'])) {
             $user->name = $data['name'];
-        }
-
-        if (isset($data['email'])) {
-            $user->email = $data['email'];
-        }
-
-        if (!empty($data['password'])) {
-            $user->password = bcrypt($data['password']);
-        }
-
-        if (isset($data['phone_number'])) {
-            $user->phone_number = $data['phone_number'];
         }
 
         $user->save();
