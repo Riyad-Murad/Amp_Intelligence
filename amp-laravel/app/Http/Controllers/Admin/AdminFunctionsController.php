ed27c0c8d12d60d6d8b75731c154a221e3042bcf
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 03:28:00 +0300

    Admin_Controller_Functions: Added controller edit Provider function

--- amp-laravel/app/Http/Controllers/Admin/AdminFunctionsController.php
+++ amp-laravel/app/Http/Controllers/Admin/AdminFunctionsController.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\EditProfileRequest;
 use App\Http\Requests\Admin\EditProviderRequest;
 use App\Services\Admin\editProfileService;
-use App\Services\Admin\AdminEditProfileService;
 use App\Services\Admin\getAllProvidersService;
 use App\Services\Admin\DeleteContactMessageService;
 use App\Services\Admin\getAllContactMessagesService;
+use App\Services\Admin\AdminEditProviderProfileService;
 
 class AdminFunctionsController extends Controller
 {
@@ -23,7 +23,13 @@ class AdminFunctionsController extends Controller
     }
 
     public function editProvider(EditProviderRequest $request) {
+        try {
+            AdminEditProviderProfileService::editProfile($request->validated());
 
+            return $this->messageResponse(true, "Profile updated successfully", 200);
+        } catch (\Exception $e) {
+            return $this->errorMessageResponse(false, $e->getMessage(), "Failed to update profile", 500);
+        }
     }
 
     public function editProfile(EditProfileRequest $request) {
