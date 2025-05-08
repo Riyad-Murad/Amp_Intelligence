fbcb3a815d5a3ff252b626fa3c7b0208954728dc
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 03:45:28 +0300

    Admin_Controller_Functions: Added condition to check if "password" is found in the request to change it

--- amp-laravel/app/Services/Admin/AdminEditProviderProfileService.php
+++ amp-laravel/app/Services/Admin/AdminEditProviderProfileService.php
@@ -18,6 +18,10 @@ class AdminEditProviderProfileService
             $user->email = $data['email'];
         }
 
+        if (!empty($data['password'])) {
+            $user->password = bcrypt($data['password']);
+        }
+
         $user->save();
 
         return $user;
