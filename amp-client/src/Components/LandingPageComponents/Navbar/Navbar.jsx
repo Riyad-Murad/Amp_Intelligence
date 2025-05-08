f3ecb609a9df84f5d36d355c0688406545a4427b
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 23:47:18 +0300

    landing_page_navbar_dev: Imported ActionButton and logo components

--- amp-client/src/Components/LandingPageComponents/Navbar/Navbar.jsx
+++ amp-client/src/Components/LandingPageComponents/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import "./styles.css"
+import React from "react";
+import "./styles.css";
+import ActionButton from "../ActionButton/ActionButton";
+import logo from "../../../assets/logo.png";
 
 const Navbar = () => {
   return (
     <div>Navbar</div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
