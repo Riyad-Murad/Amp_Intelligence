3580fdf7b04f611c526b494deab41f757938463c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 01:09:18 +0300

    landing_page_navbar_dev: Modified links

--- amp-client/src/Components/LandingPageComponents/Navbar/Navbar.jsx
+++ amp-client/src/Components/LandingPageComponents/Navbar/Navbar.jsx
@@ -1,19 +1,22 @@
-import React from "react";
 import "./styles.css";
-import ActionButton from "../ActionButton/ActionButton";
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
+import ActionButton from "../ActionButton/ActionButton";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="Logo" className="logo" />
-        <a href="/" className="navbar-title">Amp Intelligence</a>
+        <Link href="/" className="navbar-title">
+          Amp Intelligence
+        </Link>
       </div>
       <div className="navbar-links">
-        <a href="#">Services</a>
-        <a href="#">About Us</a>
-        <a href="#">Contact Us</a>
+        <a href="#services">Services</a>
+        <a href="#about-us">About Us</a>
+        <a href="#contact-us">Contact Us</a>
       </div>
       <div className="navbar-right">
         <ActionButton backgroundColor="#233A7E" color="#FFFFFF" text="Login" />
