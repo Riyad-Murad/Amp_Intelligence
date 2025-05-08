f13614f872a677f5ff7884c3506955a2649d3d99
Riyad Murad <riyadmurad01@gmail.com>
2025-05-08 03:20:38 +0300

    Admin_Controller_Functions: Added condition to check if "name" is found in the request to change it

--- amp-laravel/app/Services/Admin/editProfileService.php
+++ amp-laravel/app/Services/Admin/editProfileService.php
@@ -7,6 +7,14 @@ use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 class editProfileService
 {
     public static function editProfile(array $data){
+        $user = JWTAuth::user();
+
+        if (isset($data['name'])) {
+            $user->name = $data['name'];
+        }
         
+        $user->save();
+
+        return $user;
     }
 }
