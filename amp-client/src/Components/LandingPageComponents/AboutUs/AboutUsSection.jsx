bc1539f0ba24bf5d40973a51474203ae5d47bed7
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 02:16:07 +0300

    landing_about_us_section_dev: Added first section of the About Us section

---
+++ amp-client/src/Components/LandingPageComponents/AboutUs/AboutUsSection.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React from "react";
+
+const AboutUsSection = () => {
+  return (
+    <section className="about-us-section">
+      <h2 className="section-titles primary-color">About Us</h2>
+      <p className="about-us-text">
+        At Amp Intelligence, we are driven by a passion for innovation and a
+        commitment to providing exceptional solutions in the realm of electrical
+        usage management. Founded on the principles of efficiency, reliability,
+        and sustainability, we strive to empower individuals and businesses with
+        the tools and insights they need to optimize their energy consumption.
+      </p>
+    </section>
+  );
+};
+
+export default AboutUsSection;
