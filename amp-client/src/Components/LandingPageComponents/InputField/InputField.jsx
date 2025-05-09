f12b7bef5d397b275ef60e334c6fa29e0714a7fd
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 13:47:18 +0300

    landing_contact_us_section_dev: Added InputField Component Code

--- amp-client/src/Components/LandingPageComponents/InputField/InputField.jsx
+++ amp-client/src/Components/LandingPageComponents/InputField/InputField.jsx
@@ -1,10 +1,26 @@
 import './styles.css'
 import React from 'react'
 
-const InputField = () => {
+const InputField = ({ label, placeholder, value, onChange, width, textarea }) => {
   return (
-    <div>InputField</div>
-  )
+    <div className="input-field-container" style={{ width: width || '100%' }}>
+      <label className="subtitle black-color">{label}*</label>
+      {textarea ? (
+        <textarea
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      ) : (
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      )}
+    </div>
+  );
 }
 
 export default InputField
