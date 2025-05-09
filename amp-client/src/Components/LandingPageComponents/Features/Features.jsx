b11a01e37401967ea8fd4bea5201767f4f8701b8
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 03:49:27 +0300

    landing_features_section_dev: Added featuresData to include them in the cards

--- amp-client/src/Components/LandingPageComponents/Features/Features.jsx
+++ amp-client/src/Components/LandingPageComponents/Features/Features.jsx
@@ -1,10 +1,26 @@
-import './styles.css'
-import React from 'react'
+import "./styles.css";
+import React from "react";
+import FeatureCard from "../FeatureCard/FeatureCard";
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+  const featuresData = [
+    {
+      title: "Real-time Monitoring",
+      text: "Gain instant insights into your electrical usage with our intuitive dashboard.",
+    },
+    {
+      title: "Smart Automation",
+      text: "Automate provisioning and management tasks to optimize efficiency.",
+    },
+    {
+      title: "Detailed Analytics",
+      text: "Analyze historical data to identify trends and make informed decisions.",
+    },
+  ];
 
-export default Features
+  return(
+    <div>Features</div>
+  );
+};
+
+export default Features;
