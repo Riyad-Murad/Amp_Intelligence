1ec01650a250af5377f75d0f68002b98e605546a
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 04:13:44 +0300

    landing_pricing_section_dev: Added PricesSection Component to Home landing Page

--- amp-client/src/Pages/Home/Home.jsx
+++ amp-client/src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../Components/LandingPageComponents/Navbar/Navbar";
 import Features from "../../Components/LandingPageComponents/Features/Features";
 import HeroSection from "../../Components/LandingPageComponents/HeroSection/HeroSection";
 import AboutUsSection from "../../Components/LandingPageComponents/AboutUs/AboutUsSection";
+import PricesSection from "../../Components/LandingPageComponents/PricesSection/PricesSection";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <HeroSection />
       <AboutUsSection />
       <Features />
+      <PricesSection />
     </>
   );
 };
