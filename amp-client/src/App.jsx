20800eaa755e48b647854078a6dea25baa238341
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 21:01:27 +0300

    client_navbar_and_pages_dev: Imported ClientPowerPlan Component into App Routes

--- amp-client/src/App.jsx
+++ amp-client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './Pages/CommonPages/Home/Home'
 import { Route, Routes } from 'react-router-dom'
 import ClientDashboard from './Pages/ClientPages/Dashboard/ClientDashboard'
+import ClientPowerPlan from './Pages/ClientPages/PowerPlan/ClientPowerPlan'
 // import ProtectedRoute from './Components/CommonComponents/ProtectedRoutes/ProtectedRoute'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       {/* <Route element={<ProtectedRoute />}> */}
         <Route path='/client-dashboard' element={<ClientDashboard />} />
+        <Route path='/client-power-plan' element={<ClientPowerPlan />} />
       {/* </Route> */}
     </Routes>
   )
