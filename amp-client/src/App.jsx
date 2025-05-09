e529a2a97460e0f94dd010a3e56c10e48c68da9b
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 21:00:50 +0300

    client_navbar_and_pages_dev: Imported ClientDashboard Component into App Routes

--- amp-client/src/App.jsx
+++ amp-client/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import Home from './Pages/Home/Home'
+import Home from './Pages/CommonPages/Home/Home'
 import { Route, Routes } from 'react-router-dom'
-
+import ClientDashboard from './Pages/ClientPages/Dashboard/ClientDashboard'
+// import ProtectedRoute from './Components/CommonComponents/ProtectedRoutes/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route element={<ProtectedRoute />}>
-
-      </Route> */}
+      {/* <Route element={<ProtectedRoute />}> */}
+        <Route path='/client-dashboard' element={<ClientDashboard />} />
+      {/* </Route> */}
     </Routes>
   )
 }
