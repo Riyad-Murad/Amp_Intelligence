e2d75751077966189d0c6353314ed7c5d31e7a29
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 02:39:34 +0300

    admin_sidebar_and_pages_dev: Imported the needed variables for the sidebar component

--- amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
+++ amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
@@ -1,4 +1,14 @@
 import './styles.css'
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+import LogoutButton from "../../CommonComponents/LogoutButton/LogoutButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTachometerAlt,
+  faUsers,
+  faBolt,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const AdminSidebar = () => {
   return (
