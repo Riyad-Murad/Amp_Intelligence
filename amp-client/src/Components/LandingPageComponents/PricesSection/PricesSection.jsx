1f62bda69fc6062b608686d300bd0fb7ea7e261c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 04:48:09 +0300

    landing_pricing_section_dev: Added return form for PricesSection Component filling PriceCard data

--- amp-client/src/Components/LandingPageComponents/PricesSection/PricesSection.jsx
+++ amp-client/src/Components/LandingPageComponents/PricesSection/PricesSection.jsx
@@ -25,9 +25,26 @@ function PricesSection() {
     },
   ];
 
-  return(
-    <div>PricesSection</div>
-  )
+  return (
+    <section className="prices-section">
+      <h2 className="section-titles primary-color">Pricing</h2>
+      <p className="text black-color">
+        These are the different kinds of services that we offer based on your
+        needs. If you are interested in any of our services, don't hesitate to
+        contact us via phone, email or by filling out the contact form.
+      </p>
+      <div className="prices-grid">
+        {pricingData.map((plan, index) => (
+          <PriceCard
+            key={index}
+            title={plan.title}
+            features={plan.features}
+            price={plan.price}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default PricesSection;
