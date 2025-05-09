2da5e65ac47aa718ad0b36cccb1bdbd1b15906a4
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 00:38:12 +0300

    provider_sidebar_and_pages_dev: Added jsx file for LogoutButton Common Component

--- amp-client/src/Components/CommonComponents/LogoutButton/LogoutButton.jsx
+++ amp-client/src/Components/CommonComponents/LogoutButton/LogoutButton.jsx
@@ -1,9 +1,14 @@
-import './styles.css'
+import "./styles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const LogoutButton = () => {
   return (
-    <div>LogoutButton</div>
-  )
-}
+    <button className="logout-button">
+      <FontAwesomeIcon icon={faSignOutAlt} className="logout-icon" />
+      Logout
+    </button>
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
