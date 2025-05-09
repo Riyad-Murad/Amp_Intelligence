e1da3bae8d7cb0e53b2eeb192639504b28806f4d
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 02:45:36 +0300

    admin_sidebar_and_pages_dev: Added AdminSidebar Component Code

--- amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
+++ amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
@@ -1,4 +1,4 @@
-import './styles.css'
+import "./styles.css";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 import LogoutButton from "../../CommonComponents/LogoutButton/LogoutButton";
@@ -12,8 +12,38 @@ import {
 
 const AdminSidebar = () => {
   return (
-    <div>AdminSidebar</div>
-  )
-}
+    <div className="admin-sidebar">
+      <div className="sidebar-header">
+        <Link to="/admin-dashboard" className="un-underlined">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+        <Link to="/admin-dashboard" className="un-underlined">
+          <h1 className="navbar-title">Amp Intelligence</h1>
+        </Link>
+      </div>
+      <nav className="sidebar-nav">
+        <Link to="/admin-navigation-page" className="sidebar-link">
+          <FontAwesomeIcon icon={faTachometerAlt} className="sidebar-icon" />
+          <span className="link-text">Navigation Page</span>
+        </Link>
+        <Link to="/admin-add-provider" className="sidebar-link">
+          <FontAwesomeIcon icon={faUsers} className="sidebar-icon" />
+          <span className="link-text">Add Provider</span>
+        </Link>
+        <Link to="/admin-view-providers" className="sidebar-link">
+          <FontAwesomeIcon icon={faBolt} className="sidebar-icon" />
+          <span className="link-text">View Providers</span>
+        </Link>
+        <Link to="/admin-contact-messages" className="sidebar-link">
+          <FontAwesomeIcon icon={faUser} className="sidebar-icon" />
+          <span className="link-text">Contact Messages</span>
+        </Link>
+      </nav>
+      <div className="sidebar-footer">
+        <LogoutButton />
+      </div>
+    </div>
+  );
+};
 
-export default AdminSidebar
+export default AdminSidebar;
