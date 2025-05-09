f70dcf84a6520373a34252043e959f7ad072a66c
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 21:02:50 +0300

    client_navbar_and_pages_dev: Created jsx file for ProtectedRoutes Component and added Component Code

---
+++ amp-client/src/Components/CommonComponents/ProtectedRoutes/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Outlet } from "react-router-dom";
+import AdminSidebar from "../../AdminComponents/AdminSidebar/AdminSidebar";
+import ClientNavbar from "../../ClientComponents/ClientNavbar/ClientNavbar";
+import ProviderSidebar from "../../ProviderComponents/ProviderSidebar/ProviderSidebar";
+
+const ProtectedRoute = () => {
+  const isAuthenticated = localStorage.getItem("Token");
+  const userType = localStorage.getItem("user_type");
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  if (userType === "Client") {
+    return (
+      <>
+        <ClientNavbar />
+        <Outlet />
+      </>
+    );
+  }
+};
+
+export default ProtectedRoute;
