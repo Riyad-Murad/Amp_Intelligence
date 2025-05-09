0f0dd3d9fa674bd92a6d45e0c14432d7697f3151
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 13:43:50 +0300

    landing_contact_us_section_dev: Added ContactUsSection Component to Home landing Page

--- amp-client/src/Pages/Home/Home.jsx
+++ amp-client/src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Features from "../../Components/LandingPageComponents/Features/Features";
 import HeroSection from "../../Components/LandingPageComponents/HeroSection/HeroSection";
 import AboutUsSection from "../../Components/LandingPageComponents/AboutUs/AboutUsSection";
 import PricesSection from "../../Components/LandingPageComponents/PricesSection/PricesSection";
+import ContactUsSection from "../../Components/LandingPageComponents/ContactUs/ContactUsSection";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <AboutUsSection />
       <Features />
       <PricesSection />
+      <ContactUsSection />
     </>
   );
 };
