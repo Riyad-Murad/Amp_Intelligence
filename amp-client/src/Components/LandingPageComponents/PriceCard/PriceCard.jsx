35fc06fd8b13cdca06cf435ea38f13a2ef9933c7
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 04:43:48 +0300

    landing_pricing_section_dev: Added PriceCard Component Code

--- amp-client/src/Components/LandingPageComponents/PriceCard/PriceCard.jsx
+++ amp-client/src/Components/LandingPageComponents/PriceCard/PriceCard.jsx
@@ -1,10 +1,20 @@
-import './styles.css'
-import React from 'react'
+import "./styles.css";
+import React from "react";
 
-const PriceCard = () => {
+function PriceCard({ title, features, price }) {
   return (
-    <div>PriceCard</div>
-  )
+    <div className="price-card sub-sections">
+      <h3 className="section-titles primary-color">{title}</h3>
+      <ul>
+        {features.map((feature, index) => (
+          <li key={index} className="text black-color">
+            {feature}
+          </li>
+        ))}
+      </ul>
+      <p className="subtitle black-color">Price: ${price}</p>
+    </div>
+  );
 }
 
-export default PriceCard
+export default PriceCard;
