a090b2a42f812b9c719ef3633ae1318f91039a9f
Riyad Murad <riyadmurad01@gmail.com>
2025-05-09 04:47:13 +0300

    landing_pricing_section_dev: Added PricesSection Component pricingData Code to be filled inside the Pricing Cards

--- amp-client/src/Components/LandingPageComponents/PricesSection/PricesSection.jsx
+++ amp-client/src/Components/LandingPageComponents/PricesSection/PricesSection.jsx
@@ -1,10 +1,33 @@
-import './styles.css'
-import React from 'react'
+import "./styles.css";
+import React from "react";
+import PriceCard from "../PriceCard/PriceCard";
 
-const PricesSection = () => {
-  return (
+function PricesSection() {
+  const pricingData = [
+    {
+      title: "Basic Plan",
+      features: ["Feature 1", "Feature 2", "Limited Support"],
+      price: 19,
+    },
+    {
+      title: "Standard Plan",
+      features: ["All Basic Features", "Enhanced Support", "More Options"],
+      price: 49,
+    },
+    {
+      title: "Premium Plan",
+      features: [
+        "All Standard Features",
+        "Priority Support",
+        "Advanced Analytics",
+      ],
+      price: 99,
+    },
+  ];
+
+  return(
     <div>PricesSection</div>
   )
 }
 
-export default PricesSection
+export default PricesSection;
