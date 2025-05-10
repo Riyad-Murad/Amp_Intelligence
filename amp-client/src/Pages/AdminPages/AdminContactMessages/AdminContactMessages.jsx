a6a3f8186caa49d07bd98dc6808f96d4df70beff
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 03:09:04 +0300

    admin_sidebar_and_pages_dev: Added AdminContactMessages Component Code

--- amp-client/src/Pages/AdminPages/AdminContactMessages/AdminContactMessages.jsx
+++ amp-client/src/Pages/AdminPages/AdminContactMessages/AdminContactMessages.jsx
@@ -1,9 +1,17 @@
-import './styles.css'
+import "./styles.css";
+import AdminSidebar from "../../../Components/AdminComponents/AdminSidebar/AdminSidebar";
 
 const AdminContactMessages = () => {
   return (
-    <div>AdminContactMessages</div>
-  )
-}
+    <>
+      <div className="admin-contact-messages-container">
+        <AdminSidebar />
+        <div className="main-content">
+          <h1 className="main-content-title section-titles"></h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AdminContactMessages
+export default AdminContactMessages;
