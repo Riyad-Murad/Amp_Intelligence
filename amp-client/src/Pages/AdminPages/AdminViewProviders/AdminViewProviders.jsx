3e5105e3d162202a384337d811700346da7b8761
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 03:09:38 +0300

    admin_sidebar_and_pages_dev: Added AdminViewProviders Component Code

--- amp-client/src/Pages/AdminPages/AdminViewProviders/AdminViewProviders.jsx
+++ amp-client/src/Pages/AdminPages/AdminViewProviders/AdminViewProviders.jsx
@@ -1,9 +1,17 @@
-import './styles.css'
+import "./styles.css";
+import AdminSidebar from "../../../Components/AdminComponents/AdminSidebar/AdminSidebar";
 
 const AdminViewProviders = () => {
   return (
-    <div>AdminViewProviders</div>
-  )
-}
+    <>
+      <div className="admin-view-providers-container">
+        <AdminSidebar />
+        <div className="main-content">
+          <h1 className="main-content-title section-titles"></h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AdminViewProviders
+export default AdminViewProviders;
