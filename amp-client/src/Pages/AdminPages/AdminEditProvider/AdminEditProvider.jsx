2f9c9f09f674790f507a168cbba3224ba1da5bd2
Riyad Murad <riyadmurad01@gmail.com>
2025-05-10 03:01:37 +0300

    admin_sidebar_and_pages_dev: Added AdminEditProvider Component Code

---
+++ amp-client/src/Pages/AdminPages/AdminEditProvider/AdminEditProvider.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+import AdminSidebar from "../../../Components/AdminComponents/AdminSidebar/AdminSidebar";
+
+const AdminAddProvider = () => {
+  return (
+    <>
+      <div className="provider-users-container">
+        <AdminSidebar />        
+        <div className="main-content">
+          <h1 className="main-content-title section-titles"></h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminAddProvider;
