5af7bb4517616a097aa820e833e0fb82f2928363
Riyad Murad <riyadmurad01@gmail.com>
2025-05-12 01:20:46 +0300

    redux_implmentation: Created store file for my redux

---
+++ amp-client/src/Redux/store.js
@@ -0,0 +1,39 @@
+import { combineReducers } from "redux";
+import userSlice from "./Slices/UserSlice";
+import storage from "redux-persist/lib/storage";
+import loadingSlice from "./Slices//loadingSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  loading: loadingSlice,
+  user: userSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
