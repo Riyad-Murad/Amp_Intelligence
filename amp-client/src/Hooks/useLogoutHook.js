3195f79cc886d3a4b296f79d94cadb2f62b256b0
Riyad Murad <riyadmurad01@gmail.com>
2025-05-12 03:00:49 +0300

    client_pages_code_dev: Added useLogoutHook.js Code

---
+++ amp-client/src/Hooks/useLogoutHook.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+import axiosBaseUrl from "../Axios/axios";
+import { useDispatch } from "react-redux";
+import { persistor } from "../Redux/store";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../Redux/Slices/UserSlice";
+
+export const useLogout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axiosBaseUrl.post("/logout");
+      if (response.data.success === true) {
+        localStorage.clear();
+        await persistor.purge();
+        dispatch(logout());
+        navigate("/");
+      } else {
+        toast.error("Failed to Logout");
+      }
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+  return handleLogout;
+};
