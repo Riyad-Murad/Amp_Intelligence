697cd6d4c3ec9e79337c01d0515fb46e44847706
Riyad Murad <riyadmurad01@gmail.com>
2025-05-12 23:27:09 +0300

    provider_pages_code_dev: Removed the ProviderSidebar component

--- amp-client/src/Pages/ProviderPages/ProviderUsers/ProviderUsers.jsx
+++ amp-client/src/Pages/ProviderPages/ProviderUsers/ProviderUsers.jsx
@@ -1,11 +1,9 @@
 import "./styles.css";
-import ProviderSidebar from "../../../Components/ProviderComponents/ProviderSidebar/ProviderSidebar";
 
 const ProviderUsers = () => {
   return (
     <>
       <div className="provider-users-container">
-        <ProviderSidebar />
         <div className="main-content">
           <h1 className="main-content-title section-titles">Users</h1>
         </div>
