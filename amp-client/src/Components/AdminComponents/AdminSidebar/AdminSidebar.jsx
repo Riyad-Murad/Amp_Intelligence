a506ba29062affe10be3ca775088025f8d249214
Riyad Murad <riyadmurad01@gmail.com>
2025-05-12 23:37:46 +0300

    provider_pages_code_dev: Fixed pathing for Link

--- amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
+++ amp-client/src/Components/AdminComponents/AdminSidebar/AdminSidebar.jsx
@@ -4,37 +4,37 @@ import logo from "../../../assets/logo.png";
 import LogoutButton from "../../CommonComponents/LogoutButton/LogoutButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faFileAlt,       // Navigation Page
-    faUserEdit,    // Add Provider
-    faEye,         // View Providers
-    faEnvelope      // Contact Messages
+    faFileAlt, 
+    faUserEdit,
+    faEye,     
+    faEnvelope 
 } from "@fortawesome/free-solid-svg-icons";
 
 const AdminSidebar = () => {
   return (
     <div className="admin-sidebar">
       <div className="sidebar-header">
-        <Link to="/admin-navigation-page" className="un-underlined">
+        <Link to="/admin/navigation-page" className="un-underlined">
           <img src={logo} alt="Logo" className="logo" />
         </Link>
-        <Link to="/admin-navigation-page" className="un-underlined">
+        <Link to="/admin/navigation-page" className="un-underlined">
           <h1 className="navbar-title">Amp Intelligence</h1>
         </Link>
       </div>
       <nav className="sidebar-nav">
-        <Link to="/admin-navigation-page" className="sidebar-link">
+        <Link to="/admin/navigation-page" className="sidebar-link">
           <FontAwesomeIcon icon={faFileAlt} className="sidebar-icon" />
           <span className="link-text">Navigation Page</span>
         </Link>
-        <Link to="/admin-edit-provider" className="sidebar-link">
+        <Link to="/admin/edit-provider" className="sidebar-link">
           <FontAwesomeIcon icon={faUserEdit} className="sidebar-icon" />
           <span className="link-text">Edit Provider</span>
         </Link>
-        <Link to="/admin-view-providers" className="sidebar-link">
+        <Link to="/admin/view-providers" className="sidebar-link">
           <FontAwesomeIcon icon={faEye} className="sidebar-icon" />
           <span className="link-text">View Providers</span>
         </Link>
-        <Link to="/admin-contact-messages" className="sidebar-link">
+        <Link to="/admin/contact-messages" className="sidebar-link">
           <FontAwesomeIcon icon={faEnvelope} className="sidebar-icon" />
           <span className="link-text">Contact Messages</span>
         </Link>
