dfd5dc8d4d967ed74e8f81882e7c69e09fe5049f
Riyad Murad <riyadmurad01@gmail.com>
2025-05-14 01:50:30 +0300

    landing_page_adjustments_dev: Added the icons in the FeaturesSection component in the FeatureCards

--- amp-client/src/Components/LandingPageComponents/Features/Features.jsx
+++ amp-client/src/Components/LandingPageComponents/Features/Features.jsx
@@ -1,19 +1,27 @@
 import "./styles.css";
 import FeatureCard from "../FeatureCard/FeatureCard";
+import {
+  faChartLine,
+  faCogs,
+  faChartPie,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Features = ({ id }) => {
   const featuresData = [
     {
       title: "Real-time Monitoring",
       text: "Gain instant insights into your electrical usage with our intuitive dashboard.",
+      icon: faChartLine,
     },
     {
       title: "Smart Automation",
       text: "Automate provisioning and management tasks to optimize efficiency.",
+      icon: faCogs,
     },
     {
       title: "Detailed Analytics",
       text: "Analyze historical data to identify trends and make informed decisions.",
+      icon: faChartPie,
     },
   ];
 
@@ -22,7 +30,12 @@ const Features = ({ id }) => {
       <h2 className="section-titles primary-color">Features</h2>
       <div className="features-grid">
         {featuresData.map((feature, index) => (
-          <FeatureCard key={index} title={feature.title} text={feature.text} />
+          <FeatureCard
+            key={index}
+            title={feature.title}
+            text={feature.text}
+            icon={feature.icon}
+          />
         ))}
       </div>
     </section>
