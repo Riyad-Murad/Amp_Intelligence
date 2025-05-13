7ddb71bd0a09350e08959272d3b93f53d666ef0d
Riyad Murad <riyadmurad01@gmail.com>
2025-05-13 18:14:01 +0300

    provider_pages_code_dev: Added MetricSeeder code

--- amp-laravel/database/seeders/MetricSeeder.php
+++ amp-laravel/database/seeders/MetricSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Slave;
+use App\Models\Metric;
 use Illuminate\Database\Seeder;
 
 class MetricSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $slaves = Slave::all();
+
+        foreach ($slaves as $slave) {
+            $masterId = $slave->master_id;
+
+            Metric::factory(20)->create([
+                'slave_id' => $slave->id,
+                'master_id' => $masterId,
+            ]);
+        }
     }
 }
