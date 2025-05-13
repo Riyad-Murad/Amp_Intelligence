3819dda491aa2e0b10fa19174c13f3033c338978
Riyad Murad <riyadmurad01@gmail.com>
2025-05-13 15:47:15 +0300

    provider_pages_code_dev: Modified DatabaseSeeder file

--- amp-laravel/database/seeders/DatabaseSeeder.php
+++ amp-laravel/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Slave;
-use App\Models\Master;
-use App\Models\Metric;
-use App\Models\Line;
-use App\Models\ContactForm;
+// use App\Models\User;
+// use App\Models\Slave;
+// use App\Models\Master;
+// use App\Models\Metric;
+// use App\Models\Line;
+// use App\Models\ContactForm;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,45 +19,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create Users
-        $users = User::factory(10)->create();
-
-        // Create Masters for each User
-        $users->each(function ($user) {
-            $masters = Master::factory(2)->create([
-                'user_id' => $user->id,
-            ]);
-
-            $masters->each(function ($master) use ($user) {
-                // Create Slaves for each Master
-                $slaves = Slave::factory(2)->create([
-                    'master_id' => $master->id,
-                ]);
-
-                // Assign a random slave to the user (only for Clients maybe)
-                if ($user->user_type === 'Client') {
-                    $user->slave_id = $slaves->random()->id;
-                    $user->save();
-                }
-
-                // Create Metrics for each Slave
-                $slaves->each(function ($slave) use ($master) {
-                    Metric::factory(5)->create([
-                        'master_id' => $master->id,
-                        'slave_id' => $slave->id,
-                    ]);
-                });
-
-                // Create Lines for each Master
-                Line::factory(3)->create([
-                    'master_id' => $master->id,
-                ]);
-            });
-        });
-
-        // Create ContactForms for random users
-        ContactForm::factory(10)->create([
-            'user_id' => User::inRandomOrder()->first()->id,
+        $this->call([
+            UserSeeder::class,
+            MasterSeeder::class,
+            SlaveSeeder::class,
+            MetricSeeder::class,
+            LineSeeder::class,
+            ContactFormSeeder::class,
         ]);
+
+        // // Create Users
+        // $users = User::factory(10)->create();
+
+        // // Create Masters for each User
+        // $users->each(function ($user) {
+        //     $masters = Master::factory(2)->create([
+        //         'user_id' => $user->id,
+        //     ]);
+
+        //     $masters->each(function ($master) use ($user) {
+        //         // Create Slaves for each Master
+        //         $slaves = Slave::factory(2)->create([
+        //             'master_id' => $master->id,
+        //         ]);
+
+        //         // Assign a random slave to the user (only for Clients maybe)
+        //         if ($user->user_type === 'Client') {
+        //             $user->slave_id = $slaves->random()->id;
+        //             $user->save();
+        //         }
+
+        //         // Create Metrics for each Slave
+        //         $slaves->each(function ($slave) use ($master) {
+        //             Metric::factory(5)->create([
+        //                 'master_id' => $master->id,
+        //                 'slave_id' => $slave->id,
+        //             ]);
+        //         });
+
+        //         // Create Lines for each Master
+        //         Line::factory(3)->create([
+        //             'master_id' => $master->id,
+        //         ]);
+        //     });
+        // });
+
+        // // Create ContactForms for random users
+        // ContactForm::factory(10)->create([
+        //     'user_id' => User::inRandomOrder()->first()->id,
+        // ]);
     }
 }
