ccc0e2b8b5d22c29fb7d14c02288831e84e88f42
Riyad Murad <riyadmurad01@gmail.com>
2025-05-13 18:13:51 +0300

    provider_pages_code_dev: Added MasterSeeder code

--- amp-laravel/database/seeders/MasterSeeder.php
+++ amp-laravel/database/seeders/MasterSeeder.php
@@ -2,16 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Master;
 use Illuminate\Database\Seeder;
 
 class MasterSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $providers = User::where('user_type', 'Provider')->get();
+
+        foreach ($providers as $provider) {
+            Master::factory(2)->create([
+                'user_id' => $provider->id,
+            ]);
+        }
     }
 }
