b85aa35634eff0d917e583c0b1f16f4af34b879f
Riyad Murad <riyadmurad01@gmail.com>
2025-05-13 18:13:44 +0300

    provider_pages_code_dev: Added LineSeeder code

--- amp-laravel/database/seeders/LineSeeder.php
+++ amp-laravel/database/seeders/LineSeeder.php
@@ -2,16 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Line;
+use App\Models\Master;
 use Illuminate\Database\Seeder;
 
 class LineSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $masters = Master::all();
+
+        foreach ($masters as $master) {
+            Line::factory(6)->create([
+                'master_id' => $master->id,
+            ]);
+        }
     }
 }
