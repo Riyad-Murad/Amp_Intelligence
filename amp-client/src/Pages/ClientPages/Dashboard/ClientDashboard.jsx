40b69e0fe02dc60851d3fc2582f1d44e805fdf97
Riyad Murad <riyadmurad01@gmail.com>
2025-05-14 02:24:32 +0300

    landing_page_adjustments_dev: Modified parent div className name

--- amp-client/src/Pages/ClientPages/Dashboard/ClientDashboard.jsx
+++ amp-client/src/Pages/ClientPages/Dashboard/ClientDashboard.jsx
@@ -2,9 +2,9 @@ import "./styles.css";
 
 const ClientDashboard = () => {
   return (
-    <>
+    <div className="client-dashboard-container">
       <h2>Client Dashboard</h2>
-    </>
+    </div>
   );
 };
 
