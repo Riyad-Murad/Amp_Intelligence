21488e411095c3b651da8606f0dd0a53834fcd07
Riyad Murad <riyadmurad01@gmail.com>
2025-05-14 01:54:02 +0300

    landing_page_adjustments_dev: Removed commented code

--- amp-client/src/Components/LandingPageComponents/PriceCard/PriceCard.jsx
+++ amp-client/src/Components/LandingPageComponents/PriceCard/PriceCard.jsx
@@ -26,23 +26,3 @@ function PriceCard({ title, features, price, isPopular, isActive, badgeText }) {
 }
 
 export default PriceCard;
-
-// import "./styles.css";
-
-// function PriceCard({ title, features, price }) {
-//   return (
-//     <div className="price-card sub-sections">
-//       <h3 className="section-titles primary-color">{title}</h3>
-//       <ul>
-//         {features.map((feature, index) => (
-//           <li key={index} className="text black-color">
-//             {feature}
-//           </li>
-//         ))}
-//       </ul>
-//       <p className="subtitle black-color">Price: ${price}</p>
-//     </div>
-//   );
-// }
-
-// export default PriceCard;
