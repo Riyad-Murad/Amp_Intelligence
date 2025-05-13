0340fb0a2a088b3d368e4c15a872c9d5a5655f5b
Riyad Murad <riyadmurad01@gmail.com>
2025-05-14 02:24:29 +0300

    landing_page_adjustments_dev: Modified parent div className name

--- amp-client/src/Pages/ClientPages/ClientProfile/ClientProfile.jsx
+++ amp-client/src/Pages/ClientPages/ClientProfile/ClientProfile.jsx
@@ -2,9 +2,9 @@ import "./styles.css";
 
 const ClientProfile = () => {
   return (
-    <>
+    <div className="client-profile-container">
       <h2>Client Profile</h2>
-    </>
+    </div>
   );
 };
 
