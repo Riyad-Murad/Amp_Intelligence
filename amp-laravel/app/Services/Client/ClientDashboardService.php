af97ded3165fdfcb95e3a84f5a544543710ece3a
Riyad Murad <riyadmurad01@gmail.com>
2025-05-14 23:54:31 +0300

    client_dashboard_page_dev: Added variable to fetch power usage per day for the current month

--- amp-laravel/app/Services/Client/ClientDashboardService.php
+++ amp-laravel/app/Services/Client/ClientDashboardService.php
@@ -14,6 +14,16 @@ class ClientDashboardService
         $startOfMonth = (new DateTime('first day of this month', $timezone))->format('Y-m-d H:i:s');
         $endOfMonth = (new DateTime('last day of this month', $timezone))->format('Y-m-d 23:59:59');
 
+        // Fetch power usage per day for the current month
+        $powerUsagePerDayData = Metric::where('slave_id', $slaveId)
+            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->selectRaw('DATE(created_at) as date, SUM(power) as total_power')
+            ->groupByRaw('DATE(created_at)')
+            ->orderByRaw('DATE(created_at)')
+            ->get();
+
+        $powerUsagePerDay = $powerUsagePerDayData->pluck('total_power', 'date')->toArray();
+
         
     }
 }
