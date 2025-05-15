aef0f4a9c1014ca916fed18cc0e00e1bb26169db
Riyad Murad <riyadmurad01@gmail.com>
2025-05-15 23:15:12 +0300

    provider_dashboard_page_dev: Created Service "GetVoltageDistributionService"

---
+++ amp-laravel/app/Services/Provider/GetVoltageDistributionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Provider;
+
+class GetVoltageDistributionService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
