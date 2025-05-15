eeadafdcfa8d13a4430fed6e41ad882da3b9fedc
Riyad Murad <riyadmurad01@gmail.com>
2025-05-15 23:15:20 +0300

    provider_dashboard_page_dev: Created Service "GetMetricSummaryService"

---
+++ amp-laravel/app/Services/Provider/GetMetricSummaryService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Provider;
+
+class GetMetricSummaryService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
