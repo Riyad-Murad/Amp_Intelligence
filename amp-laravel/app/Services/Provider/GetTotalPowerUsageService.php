8097cd6f3d698a106cdd08c304d6a99ffc597803
Riyad Murad <riyadmurad01@gmail.com>
2025-05-15 23:21:38 +0300

    provider_dashboard_page_dev: Added code for Service "GetTotalPowerUsageService"

--- amp-laravel/app/Services/Provider/GetTotalPowerUsageService.php
+++ amp-laravel/app/Services/Provider/GetTotalPowerUsageService.php
@@ -2,13 +2,17 @@
 
 namespace App\Services\Provider;
 
+use App\Models\Master;
+use App\Models\Metric;
+
 class GetTotalPowerUsageService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function getTotalPowerUsage(int $providerId): float
     {
-        //
+        $masterIds = Master::where('user_id', $providerId)->pluck('id');
+        return round(Metric::whereIn('master_id', $masterIds)
+            ->whereYear('date_month', now()->year)
+            ->whereMonth('date_month', now()->month)
+            ->sum('power'), 2);
     }
 }
