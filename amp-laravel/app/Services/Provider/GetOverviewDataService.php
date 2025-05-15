62209bc955116053467155c7236adbd4de7094e1
Riyad Murad <riyadmurad01@gmail.com>
2025-05-15 23:14:33 +0300

    provider_dashboard_page_dev: Created Service "GetOverviewDataService"

---
+++ amp-laravel/app/Services/Provider/GetOverviewDataService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Provider;
+
+class GetOverviewDataService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
