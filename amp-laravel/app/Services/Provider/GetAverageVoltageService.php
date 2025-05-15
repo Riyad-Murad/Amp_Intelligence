bc4679c31f7082468529f37f1bb97368280483c0
Riyad Murad <riyadmurad01@gmail.com>
2025-05-15 23:21:49 +0300

    provider_dashboard_page_dev: Added code for Service "GetAverageVoltageService"

--- amp-laravel/app/Services/Provider/GetAverageVoltageService.php
+++ amp-laravel/app/Services/Provider/GetAverageVoltageService.php
@@ -2,13 +2,15 @@
 
 namespace App\Services\Provider;
 
+use App\Models\Master;
+use App\Models\Metric;
+use App\Models\User;
+
 class GetAverageVoltageService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public static function getAverageVoltage(int $providerId): float
     {
-        //
+        $masterIds = Master::where('user_id', $providerId)->pluck('id');
+        return round(Metric::whereIn('master_id', $masterIds)->avg('voltage'), 2);
     }
 }
