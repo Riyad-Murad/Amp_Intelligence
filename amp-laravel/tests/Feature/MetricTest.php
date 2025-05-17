34f209d81c970432e41af87952d0173a5cc55b98
Riyad Murad <riyadmurad01@gmail.com>
2025-05-17 19:28:49 +0300

    laravel_test_and_seeds_fix: Added a test case in the Metric Test

--- amp-laravel/tests/Feature/MetricTest.php
+++ amp-laravel/tests/Feature/MetricTest.php
@@ -8,13 +8,42 @@ use Tests\TestCase;
 
 class MetricTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function testSubmitSlaveMetricsSuccessfully(): void
+    {
+        $payload = [
+            'power' => "10",
+            'energy' => "30",
+            'voltage' => "220",
+            'current' => "20",
+            'master_id' => 1,
+            'date_month' => "05-16",
+            'slave_id' => 1
+        ];
+
+        $response = $this->postJson('http://localhost:8000/api/v1/metrics', $payload);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                "success" => true,
+                "message" => "Slave metric stored"
+            ])
+            ->assertJsonStructure([
+                "success",
+                "message",
+                "data" => [
+                    "voltage",
+                    "current",
+                    "power",
+                    "energy",
+                    "slave_id",
+                    "master_id",
+                    "date_month",
+                    "updated_at",
+                    "created_at",
+                    "id"
+                ]
+            ]);
     }
 }
