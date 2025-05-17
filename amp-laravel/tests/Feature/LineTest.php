a375649f3042767634f873bd91bfe5deafe824c6
Riyad Murad <riyadmurad01@gmail.com>
2025-05-17 19:29:19 +0300

    laravel_test_and_seeds_fix: Added a test case in the Line Test

--- amp-laravel/tests/Feature/LineTest.php
+++ amp-laravel/tests/Feature/LineTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LineTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function testSubmitMasterLinesSuccessfully(): void
+    {
+        $payload = [
+            'master_id' => 1,
+            'voltage_l1' => "220.5",
+            'voltage_l2' => "219.8",
+            'voltage_l3' => "221.1",
+            'power_l1' => "50.2",
+            'power_l2' => "49.7",
+            'power_l3' => "51.3"
+        ];
+
+        $response = $this->postJson('http://localhost:8000/api/v1/lines', $payload);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                "success" => true,
+                "message" => "Master lines data saved"
+            ])
+            ->assertJsonStructure([
+                "success",
+                "message",
+                "data" => [
+                    "voltage_l1",
+                    "voltage_l2",
+                    "voltage_l3",
+                    "power_l1",
+                    "power_l2",
+                    "power_l3",
+                    "master_id",
+                    "updated_at",
+                    "created_at",
+                    "id"
+                ]
+            ]);
     }
 }
