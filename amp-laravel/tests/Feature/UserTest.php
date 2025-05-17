7f4f3f072cf1f9f7af6366f9ad64c9a05857f0eb
Riyad Murad <riyadmurad01@gmail.com>
2025-05-17 19:22:39 +0300

    laravel_test_and_seeds_fix: Added a Logout test in the UserTest

--- amp-laravel/tests/Feature/UserTest.php
+++ amp-laravel/tests/Feature/UserTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
-// use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
@@ -55,4 +55,19 @@ class UserTest extends TestCase
             ]);
     }
 
+    public function testLogoutUserSuccessfully(): void
+    {
+        $user = User::factory()->create();
+        $token = JWTAuth::fromUser($user);
+
+        $response = $this->withHeader('Authorization', "Bearer $token")
+            ->postJson('http://localhost:8000/api/v1/logout');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                "success" => true,
+                "message" => "Logged Out Successfully",
+                "data" => null
+            ]);
+    }
 }
